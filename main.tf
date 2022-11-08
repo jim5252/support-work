33d6d56dcf8478c6f247bfb283b2e57326cd1996
Jim <jim.hamill@digital.homeoffice.gov.uk>
2022-11-08 14:30:16 +0000

    master arn

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ module "opensearch" {
   cluster_domain          = var.cluster_domain
   cluster_version         = "1.3"
   master_instance_enabled = false
-  master_user_arn         = "arn:aws:iam::795502215660:user/jim-admin"
+  master_user_arn         = "arn:aws:iam::795502215660:role/wf-ws-wss-eks-terranetes-controller-c6fkh64bcv40mr3972t0"
   warm_instance_enabled   = false
   hot_instance_type       = "t3.small.search"
   ebs_enabled             = true
