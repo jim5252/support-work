1353b24e8a1ccc2dc901123f053c36ded89e556c
Jim <jim.hamill@digital.homeoffice.gov.uk>
2022-11-17 17:16:33 +0000

    ll

--- lambda/main.tf
+++ lambda/main.tf
@@ -1,11 +1,22 @@
+locals {
+  policy_name = "lambda_role"
+}
+
+data "archive_file" "decompress_object" {
+  type        = "zip"
+  source_file = "${path.module}/lambda/s3-decompressor.py"
+}
+
 module "lambda" {
   source  = "terraform-aws-modules/lambda/aws"
   version =  "4.7.1"
 
-  source_path = "../lambda/s3-decompressor.py"
+  source_path = data.archive_file.decompress_object.source_file
   create_role =  true
+  policy_name = "lambda-role"
   handler = "s3-decompressor.lambda_handler"
   runtime = "python3.7"
   timeout = 30
+  function_name = "decompress-object"
 
 }
