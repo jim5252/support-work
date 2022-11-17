cbac8ef08e3b37b2dc3377b64f8aa73136719c95
Jim <jim.hamill@digital.homeoffice.gov.uk>
2022-11-17 16:43:40 +0000

    fix

--- lambda/main.tf
+++ lambda/main.tf
@@ -1,7 +1,7 @@
 module "lambda" {
-  source = "terraform-aws-modules/terraform-aws-lambda"
+  source  = "terraform-aws-modules/terraform-aws-lambda"
   version =  "4.7.1"
 
-  source_path: "../lambda/s3-decompressor.py"
+  source_path = "../lambda/s3-decompressor.py"
 
 }
