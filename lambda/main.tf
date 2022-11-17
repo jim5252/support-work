c057dd595de2ef8d3d9dba30de4f439285ac14a6
Jim <jim.hamill@digital.homeoffice.gov.uk>
2022-11-17 17:01:39 +0000

    l

--- lambda/main.tf
+++ lambda/main.tf
@@ -3,5 +3,9 @@ module "lambda" {
   version =  "4.7.1"
 
   source_path = "../lambda/s3-decompressor.py"
+  create_role =  true
+  handler = "s3-decompressor.lambda_handler"
+  runtime = "python3.7"
+  timeout = 30
 
 }
